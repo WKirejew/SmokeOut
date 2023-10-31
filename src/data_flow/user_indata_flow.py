f34a1920193d5523fea80fbb0c63b90dc05ecfa2
Kirejew Wojciech (STUD) <wojtasss99@gmail.com>
2023-10-31 14:54:46 +0100

    Some InData documentation and testing for JSON indata

--- src/data_flow/user_indata_flow.py
+++ src/data_flow/user_indata_flow.py
@@ -9,7 +9,17 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 uid_dag = DAG(
-    dag_id="save_user_indata",
-    start_date=dt.datetime(2023, 11, 1, 2, 0),
-    schedule_interval="@daily",
+    dag_id = "save_user_indata",
+    start_date = dt.datetime(2023, 11, 1, 2, 0),
+    schedule_interval = "@daily",
+)
+
+#Way of using PythonOperator with function
+def _function():
+    print("Something")
+
+function = PythonOperator(
+    task_id = "function",
+    python_callable = _function,
+    dag = uid_dag
 )
