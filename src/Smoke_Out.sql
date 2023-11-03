8417fbd14fc33429f7988d167da66dffd1b7ec33
Kirejew Wojciech (STUD) <wojtasss99@gmail.com>
2023-11-03 21:24:22 +0100

    Edited and troubleshooted SQL Script for generating DataBase

--- src/Smoke_Out.sql
+++ src/Smoke_Out.sql
@@ -19,7 +19,7 @@ CREATE TABLE `User` (
     `UserID` int(10),
     `username` varchar(30),
     `StartTime` datetime2(0),
-    `PlanID` int(2),
+    `PlanID` int(2)
 );
 -----------------------------------------------------------
 --
@@ -29,7 +29,7 @@ CREATE TABLE `User` (
 CREATE TABLE `Usage_Plans` (
     `PlanID` int(2),
     `Name` varchar(20),
-    `Price` money,
+    `Price` int(6)
 );
 
 -- --------------------------------------------------------
@@ -38,10 +38,10 @@ CREATE TABLE `Usage_Plans` (
 -- Structure of table `InApp_User_Data`
 --
 CREATE TABLE `InApp_User_Data` (
-    `InDataID` int
+    `InDataID` int,
     `UserID` int(10),
     `OpenPckg` datetime2(0),
-    `SwitchPckg` datetime2(0),
+    `SwitchPckg` datetime2(0)
 );
 
 -- --------------------------------------------------------
@@ -50,12 +50,12 @@ CREATE TABLE `InApp_User_Data` (
 -- Structure of table `InAppReporting`
 --
 CREATE TABLE `InAppReporting` (
-    `ReportID` int
+    `ReportID` int,
     `UserID` int(10),
     `RepInt` tinyint,
     `PckgOpened` int(10) NULL,
     `PckgSwitch` int(10) NULL,
-    `ReportStatus` binary(10),
+    `ReportStatus` binary(10)
 );
 
 -- --------------------------------------------------------
@@ -64,12 +64,12 @@ CREATE TABLE `InAppReporting` (
 -- Structure of table `ReportStatus`
 --
 CREATE TABLE `ReportStatus` (
-    `ReportID` int
+    `ReportID` int,
     `ReportStatus` binary(10),
     `Start` datetime2(0),
     `Last` datetime2(0),
     `Current` datetime2(0),
-    `Close` binary(1),
+    `Close` binary(1)
 );
 
 -- --------------------------------------------------------
@@ -111,16 +111,10 @@ ALTER TABLE `User`
 
 -- Table `InApp_User_Data`
 ALTER TABLE `InApp_User_Data`
-    MODIFY `InDataID` int NOT NULL AUTO_INCREMENT,
-    MODIFY `UserID` int(10) NOT NULL AUTO_INCREMENT,
+    MODIFY `InDataID` int NOT NULL AUTO_INCREMENT;
 
 -- Table `InAppReporting`
 ALTER TABLE `InAppReporting`
-    MODIFY `ReportID` int NOT NULL AUTO_INCREMENT,
-    MODIFY `UserID` int(10) NOT NULL AUTO_INCREMENT;
-
--- Table `ReportStatus`
-ALTER TABLE `ReportStatus`
     MODIFY `ReportID` int NOT NULL AUTO_INCREMENT;
 
 -- --------------------------------------------------------
